36101e7005d219d48cac47013b3e0774d1d78e8b
Jonathan <jonathan@MacBook-Pro-de-Jonathan.local>
2026-04-22 08:07:32 -0400

    Remove stale root app entry and secret

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DapperApp from './Dapper.jsx'
+import DapperApp from './src/Dapper.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
